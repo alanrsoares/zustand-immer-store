7895d41751f98a7dd6dc814ad2a2d0da308ca418
Eduardo Rabelo <oieduardorabelo@gmail.com>
2022-06-09 18:07:45 +1200

    feat(tests): encapsulating testing variables in the setup function

--- __tests__/zustand-immer-store.test.tsx
+++ __tests__/zustand-immer-store.test.tsx
@@ -3,6 +3,40 @@ import { renderHook } from "@testing-library/react-hooks/dom";
 
 import { createStore } from "../src";
 
+describe("creates a store and api object", () => {
+  const { store, selectors } = setup();
+
+  it("returns counter value", () => {
+    expect(store.actions.getCounter()).toBe(1);
+  });
+
+  it("increment counter value", () => {
+    store.actions.incrementCounter();
+    expect(store.actions.getCounter()).toBe(2);
+  });
+
+  it("decrement counter value", () => {
+    store.actions.decrementCounter();
+    expect(store.actions.getCounter()).toBe(1);
+  });
+
+  it("set counter value", () => {
+    store.actions.setCounter(150);
+    expect(store.actions.getCounter()).toBe(150);
+  });
+
+  it("select isOdd value", () => {
+    const result = selectors?.isOdd(store.get().state);
+    expect(result).toBe(false);
+  });
+
+  it("select isEven value", () => {
+    store.actions.setCounter(33);
+    const result = selectors?.isEven(store.get().state);
+    expect(result).toBe(false);
+  });
+});
+
 const useCounterStore = createStore(
   { counter: 1, step: 1 },
   {
@@ -38,44 +72,9 @@ const useCounterStore = createStore(
   }
 );
 
-describe("creates a store and api object", () => {
-  const store = setup();
-  const { selectors } = useCounterStore;
-
-  it("returns counter value", () => {
-    expect(store.actions.getCounter()).toBe(1);
-  });
-
-  it("increment counter value", () => {
-    store.actions.incrementCounter();
-    expect(store.actions.getCounter()).toBe(2);
-  });
-
-  it("decrement counter value", () => {
-    store.actions.decrementCounter();
-    expect(store.actions.getCounter()).toBe(1);
-  });
-
-  it("set counter value", () => {
-    store.actions.setCounter(150);
-    expect(store.actions.getCounter()).toBe(150);
-  });
-
-  it("select isOdd value", () => {
-    const result = selectors?.isOdd(store.get().state);
-    expect(result).toBe(false);
-  });
-
-  it("select isEven value", () => {
-    store.actions.setCounter(33);
-    const result = selectors?.isEven(store.get().state);
-    expect(result).toBe(false);
-  });
-});
-
 function setup() {
   const { result } = renderHook(() => {
     return useCounterStore();
   });
-  return result.current;
+  return { store: result.current, selectors: useCounterStore.selectors };
 }
